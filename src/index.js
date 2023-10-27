abd050d2e43935fa737098523dd7195963f6209e
Eyad Al-Ahmar <eyadalahmar15@gmail.com>
2023-10-27 20:24:10 +0200

    Update no.9

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import App from './App'
 import { Provider } from 'react-redux';
 import { persistStore } from 'redux-persist';
@@ -15,9 +15,9 @@ ReactDOM.render(
    <React.StrictMode>
    <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <HashRouter>
          <App />
-      </BrowserRouter>
+      </HashRouter>
     </PersistGate>
    </Provider>
  </React.StrictMode>,
