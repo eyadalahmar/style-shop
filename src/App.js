8c60e55cd1edaabce5c38e9e8f8be82c461cb8af
Eyad Al-Ahmar <eyadalahmar15@gmail.com>
2023-10-27 20:08:40 +0200

    Update no.7

--- src/App.js
+++ src/App.js
@@ -16,16 +16,16 @@ const App = () => {
 
  return (   
        <HashRouter>
-          <Route exact path="/style-shop" element={<Home />} Component={Home}/>
-          <Route exact path="/style-shop/contact" element={<Contact />}/>
-          <Route exact path="/style-shop/purchases" element={<Purchases />} />
-          <Route exact path="/style-shop/about" element={<About />} />
-          <Route exact path='/style-shop/login' element={<Log />} />
-          <Route exact path='/style-shop/signup' element={<Sign />} />
-          <Route exact path='/style-shop/clothes' element={<Cclothes />} />
-          <Route exact path='/style-shop/shoes' element={<Cshoes />} />
-          <Route exact path='/style-shop/electronics' element={<Celectronic />} />
-          <Route exact path='/style-shop/profile' element={<Profile />} />
+          <Route exact path="/style-shop/" element={<Home />} Component={Home}/>
+          <Route exact path="/style-shop/contact/" element={<Contact />}/>
+          <Route exact path="/style-shop/purchases/" element={<Purchases />} />
+          <Route exact path="/style-shop/about/" element={<About />} />
+          <Route exact path='/style-shop/login/' element={<Log />} />
+          <Route exact path='/style-shop/signup/' element={<Sign />} />
+          <Route exact path='/style-shop/clothes/' element={<Cclothes />} />
+          <Route exact path='/style-shop/shoes/' element={<Cshoes />} />
+          <Route exact path='/style-shop/electronics/' element={<Celectronic />} />
+          <Route exact path='/style-shop/profile/' element={<Profile />} />
           <Route path='*'  element={<Nopage />} />
 
        </HashRouter>
